9c8d895c4120ba1f010eb0925c445f2a0fb0bb02
nathanielscottstevens <nathaniel.scott.stevens@gmail.com>
2017-03-16 21:56:55 -0400

    Initial commit.

---
+++ Assets/Editor/HeightMap.cs
@@ -0,0 +1,279 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+
+[System.Serializable]
+public class HeightMap : ScriptableObject
+{
+    public float[] map;
+    public int height;
+    public int width;
+
+    public float this[int x, int y]
+    {
+        get { return map[ToIndex(x, y)]; }
+        set { map[ToIndex(x, y)] = value; }
+    }
+
+    public float this[float x, float y]
+    {
+        get
+        {
+            Assert.IsTrue(x >= 0 && x <= 1);
+            Assert.IsTrue(y >= 0 && y <= 1);
+            return map[ToIndex(Mathf.RoundToInt(x * width),
+                               Mathf.RoundToInt(y * height))];
+        }
+        set
+        {
+            Assert.IsTrue(x >= 0 && x <= 1);
+            Assert.IsTrue(y >= 0 && y <= 1);
+            map[ToIndex(Mathf.RoundToInt(x * width),
+                        Mathf.RoundToInt(y * height))] = value;
+        }
+    }
+
+    public float this[int i]
+    {
+        get { return map[i]; }
+        set { value = Mathf.Clamp01(value); map[i] = value; }
+    }
+
+    // This will only work with power of 2 dimensions!
+    int ToIndex(int x, int y)
+    {
+        return (x & (width - 1)) + (y & (height - 1)) * width;
+    }
+
+    public void Init(int width, int height)
+    {
+        Assert.IsTrue(Mathf.IsPowerOfTwo(width), "Width must be a power of 2.");
+        Assert.IsTrue(Mathf.IsPowerOfTwo(height), "Height must be a power of 2.");
+        this.width = width;
+        this.height = height;
+        map = new float[width * height];
+    }
+
+    public Texture2D GetTexture()
+    {
+        Texture2D texture = new Texture2D(width, height);
+        Color[] colors = new Color[width * height];
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float sample = this[x, y];
+                colors[y * height + x] = new Color(sample, sample, sample);
+            }
+        }
+
+        texture.SetPixels(colors);
+        texture.Apply();
+        return texture;
+    }
+
+    public void SetFromTexture(Texture2D texture)
+    {
+        Color[] pixels = texture.GetPixels();
+        map = new float[pixels.Length];
+        width = texture.width;
+        height = texture.height;
+
+        for (int i = 0; i < pixels.Length; i++)
+        {
+            map[i] = pixels[i].grayscale;
+        }
+    }
+
+    public void Sine(float freq, float phase)
+    {
+        // Shift sine to [0, 1] range: sin(x)/2 + 0.5
+        freq = 0.1f;
+        Mathf.Clamp(phase, 0, 2 * Mathf.PI);
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float v = (Mathf.Sin(2 * Mathf.PI * x) / 2 * 0.5f) * (Mathf.Sin(2 * Mathf.PI * y) / 2 * 0.5f);
+                this[x, y] = v;
+            }
+        }
+    }
+
+    public void AddCircle(float radius, float weight, int h, int k)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // Use equation of circle (x - h)^2 + (y - k)^2 = r^2
+                if ((x - h)*(x - h) + (y - k)*(y - k) < radius * radius)
+                {
+                    this[x, y] += weight;
+                }
+            }
+        }
+    }
+
+    public void AddCone(float radius, float weight, int h, int k)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                // Use equation of circle (x - h)^2 + (y - k)^2 = r^2
+
+                float distFromCenter = Mathf.Sqrt((x - h) * (x - h) + (y - k) * (y - k));
+                if (distFromCenter < radius)
+                {
+                    this[x, y] += weight * ((radius - distFromCenter) / radius);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Adds Perlin noise.
+    /// </summary>
+    /// <param name="scale">The amount of the perlin texture that is used.</param>
+    /// <param name="weight">The max height of the noise generated.</param>
+    /// <param name="octaves">The number of iterations at lesser weights.</param>
+    public void AddPerlin(float scale, float weight, int octaves=1)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float xCoord = (float)x / width * scale;
+                float yCoord = (float)y / height * scale;
+                float sample = 0;
+
+                float a = 1;
+                for (int i = 0; i <= octaves; i++)
+                {
+                    float mult = Mathf.Pow(2, i);
+                    sample += (a) * Mathf.PerlinNoise(mult * xCoord, mult * yCoord) * weight;
+                    a /= 2;
+                }
+
+                this[x, y] += sample;
+            }
+        }
+    }
+
+    public void MaskCircle(float radius)
+    {
+        int h = width / 2;
+        int k = height / 2;
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float distFromCenter = Mathf.Sqrt((x - h) * (x - h) + (y - k) * (y - k));
+                if (distFromCenter >= 0)
+                    this[x, y] *= ((radius - distFromCenter) / radius);
+                else
+                    this[x, y] = 0;
+            }
+        }
+    }
+
+    public void FadeEdges(int distance)
+    {
+        // Bottom
+        for (int y = 0; y < distance; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float curve = (Mathf.Sin(x) / 2f) + 0.5f;
+                this[x, y] *= ((float)y / distance) * curve;
+            }
+        }
+
+        // Top
+        for (int y = height - distance; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                this[x, y] *= ((float)height - y) / distance; 
+            }
+        }
+
+        // Left
+        for (int x = 0; x < distance; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                this[x, y] *= (float)x / distance;
+            }
+        }
+
+        // Right
+        for (int x = width - distance; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                this[x, y] *= ((float)width - x) / distance;
+            }
+        }
+    }
+
+    float JaggedCliffCurve(int x)
+    {
+        return ((Mathf.Sin(x * Random.Range(0.25f, 1f)) / 2f) + 0.5f);
+    }
+
+    float ErodedCliffCurve(int x)
+    {
+        return (Mathf.Sin(x) / 2f) + 0.5f;
+    }
+
+    public void SmoothAverage()
+    {
+        for (int y = 0; y < height; y += 2)
+        {
+            for (int x = 0; x < width; x += 2)
+            {
+                float ave = (this[x, y] +
+                             this[x, y + 1] +
+                             this[x + 1, y] +
+                             this[x + 1, y + 1])
+                             / 4f;
+                this[x, y] = ave;
+            }
+        }
+    }
+
+    public void SmoothHigh()
+    {
+        for (int y = 0; y < height; y += 2)
+        {
+            for (int x = 0; x < width; x += 2)
+            {
+                float[] values = { this[x, y],
+                                   this[x, y + 1],
+                                   this[x + 1, y],
+                                   this[x + 1, y + 1] };
+
+                this[x, y] = Mathf.Max(values);
+            }
+        }
+    }
+
+    public void SmoothLow()
+    {
+        for (int y = 0; y < height; y += 2)
+        {
+            for (int x = 0; x < width; x += 2)
+            {
+                float[] values = { this[x, y],
+                                   this[x, y + 1],
+                                   this[x + 1, y],
+                                   this[x + 1, y + 1] };
+
+                this[x, y] = Mathf.Min(values);
+            }
+        }
+    }
+}
---
+++ Assets/Editor/HeightMapGeneratorWindow.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class HeightMapGeneratorWindow : EditorWindow
+{
+    Texture2D texture;
+    HeightMap map;
+    int size = 256;
+
+    // Circle and Cone
+    float radius;
+    int h, k;
+    float circleWeight = 1;
+
+    // Perlin
+    float perlinScale = 1;
+    float perlinWeight = 1;
+    int perlinOctaves = 1;
+
+    // Fading
+    int fadeRadius = 1;
+
+    [MenuItem("Window/Height Map")]
+    public static void Init()
+    {
+        HeightMapGeneratorWindow window = GetWindow<HeightMapGeneratorWindow>();
+        window.titleContent = new GUIContent("Height Map");
+    }
+
+    void OnEnable()
+    {
+        map = ScriptableObject.CreateInstance<HeightMap>();
+        map.Init(size, size);
+        texture = new Texture2D(size, size);
+        Undo.undoRedoPerformed += UndoRedoCallback;
+    }
+
+    void OnGUI()
+    {
+        EditorGUI.BeginChangeCheck();
+        EditorGUILayout.LabelField("Texture Generation");
+        size = EditorGUILayout.IntPopup(size, new string[] { "32", "64", "128", "256", "512", "1024" }, new int[] { 32, 64, 128, 256, 512, 1024 }, GUILayout.Width(100));
+        if (EditorGUI.EndChangeCheck())
+        {
+            texture = new Texture2D(size, size);
+            map = ScriptableObject.CreateInstance<HeightMap>();
+            map.Init(size, size);
+        }
+
+        // Save as png, the height map object is 10x smaller
+        if (GUILayout.Button("Save"))
+        {
+            string path = AssetDatabase.GenerateUniqueAssetPath("Assets/Height Maps/Map.png");
+            byte[] bytes = texture.EncodeToPNG();
+            File.WriteAllBytes(path, bytes);
+            AssetDatabase.Refresh();
+        }
+
+        EditorGUILayout.LabelField(new GUIContent(texture), GUILayout.Width(size), GUILayout.Height(size));
+
+        EditorGUILayout.Separator();
+
+        using (var horizontal = new EditorGUILayout.HorizontalScope())
+        {
+            using (var vertical = new EditorGUILayout.VerticalScope(GUILayout.MaxWidth(300)))
+            {
+                EditorGUILayout.LabelField("Draw Circle", EditorStyles.largeLabel);
+                radius = EditorGUILayout.Slider("Radius", radius, 0, size / 1.5f);
+                EditorGUILayout.LabelField("Center");
+                h = EditorGUILayout.IntSlider("x", h, 0, size);
+                k = EditorGUILayout.IntSlider("y", k, 0, size);
+                circleWeight = EditorGUILayout.Slider("Weight", circleWeight, 0, 1);
+
+                if (GUILayout.Button("Add Circle"))
+                {
+                    Undo.RecordObject(map, "Add Circle");
+                    map.AddCircle(radius, circleWeight, h, k);
+                    texture = map.GetTexture();
+                }
+
+                if (GUILayout.Button("Add Cone"))
+                {
+                    Undo.RecordObject(map, "Add Cone");
+                    map.AddCone(radius, circleWeight, h, k);
+                    texture = map.GetTexture();
+                }
+            }
+
+            EditorGUILayout.Separator();
+
+            using (var vertical = new EditorGUILayout.VerticalScope(GUILayout.MaxWidth(300)))
+            {
+                EditorGUILayout.LabelField("Perlin Noise", EditorStyles.largeLabel);
+
+                perlinScale = EditorGUILayout.Slider("Scale", perlinScale, 0.1f, 15);
+                perlinWeight = EditorGUILayout.Slider("Weight", perlinWeight, 0.01f, 1f);
+                perlinOctaves = EditorGUILayout.IntSlider("Octaves", perlinOctaves, 1, 15);
+
+                if (GUILayout.Button("Add Perlin"))
+                {
+                    Undo.RecordObject(map, "Add Perlin");
+                    map.AddPerlin(perlinScale, perlinWeight, perlinOctaves);
+                    texture = map.GetTexture();
+                }
+            }
+        }
+
+        EditorGUILayout.Separator();
+
+        using (var vertical = new EditorGUILayout.VerticalScope(GUILayout.MaxWidth(300)))
+        {
+            EditorGUILayout.LabelField("Masking", EditorStyles.largeLabel);
+            fadeRadius = EditorGUILayout.IntSlider("Radius", fadeRadius, 1, map.width / 2);
+
+            if (GUILayout.Button("Circle"))
+            {
+                Undo.RecordObject(map, "Circle Mask");
+                map.MaskCircle(fadeRadius);
+                texture = map.GetTexture();
+            }
+        }
+    }
+
+    void UndoRedoCallback()
+    {
+        texture = map.GetTexture();
+        Repaint();
+    }
+}
